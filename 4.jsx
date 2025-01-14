0e56e7fb6c50011c219336a4f8503816806337da
ElamanBaiguanishev <elaman1007@mail.ru>
2025-01-14 12:38:10 +0600

    4 task complete

--- 4.jsx
+++ 4.jsx
@@ -12,31 +12,23 @@ import { useState, useEffect } from 'react';
 
 const Component = () => {
   const [count, setCount] = useState(0);
-  const [isGreaterThan5, setIsGreaterThan5] = useState(false);
 
   useEffect(() => {
     if (count > 5) {
-      setIsGreaterThan5(true);
-    } else {
-      setIsGreaterThan5(false);
-    }
-  }, [count]);
-
-  if (count > 5) {
-    useEffect(() => {
       const handleResize = () => {
         console.log('Resize event detected');
       };
+
       window.addEventListener('resize', handleResize);
-    }, []);
-  }
+    }
+  }, [count > 5]);
 
   return (
     <div>
       <p>Счетчик: {count}</p>
-      <button onClick={() => setCount(count + 1)}>Увеличить</button>
-      <button onClick={() => setCount(count - 1)}>Уменьшить</button>
-      {isGreaterThan5 && <p>Счетчик больше 5</p>}
+      <button onClick={() => setCount((prev) => prev + 1)}>Увеличить</button>
+      <button onClick={() => setCount((prev) => prev - 1)}>Уменьшить</button>
+      {count > 5 && <p>Счетчик больше 5</p>}
     </div>
   );
 };
