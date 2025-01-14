46a9caefedb95a6920dbd8af8ff8155763cc7a29
Andrinat <Nursultan2000@yandex.ru>
2025-01-14 11:43:12 +0500

    3 сделано

--- 3.tsx
+++ 3.tsx
@@ -4,28 +4,33 @@
 */
 
 interface IUser {
-  // Типизируйте объект User
+  id: number;
+  name: string;
+  age: number;
 }
 
 // Типизируйте компонент UserSearch
 const UserSearch = () => {
-  const users = [
-    { id: 1, name: 'Иван', age: 25 },
-    { id: 2, name: 'Мария', age: 30 },
-    { id: 3, name: 'Петр', age: 28 },
-    { id: 4, name: 'Алексей', age: 35 },
-    { id: 5, name: 'Анна', age: 22 },
+  const users: IUser[] = [
+    { id: 1, name: "Иван", age: 25 },
+    { id: 2, name: "Мария", age: 30 },
+    { id: 3, name: "Петр", age: 28 },
+    { id: 4, name: "Алексей", age: 35 },
+    { id: 5, name: "Анна", age: 22 },
   ];
 
-  const [searchTerm, setSearchTerm] = useState</*типизируйте useState*/>('');
-  const filteredUsers = []; // Фильтруйте пользователей по имени
-
+  const [searchTerm, setSearchTerm] = useState<string>("");
+  const filteredUsers = users.filter((user) => user.name === searchTerm);
   return (
     <div>
       <h1>Поиск пользователей</h1>
-      {/* Здесь напишите input для посика */}
+      <input onChange={(e) => setSearchTerm(e.target.value)} type="text" />
 
-      <ul>{/* Выводите список отфильтрованных пользователей здесь */}</ul>
+      <ul>
+        {filteredUsers.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
     </div>
   );
 };
