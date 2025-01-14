9a3922e13f6894f66db37c91417fa4b875a07602
Abaijan <fghf09647@gmail.com>
2025-01-14 12:45:25 +0600

    second task is ready

--- 2.ts
+++ 2.ts
@@ -2,11 +2,14 @@
 // Типизируйте данные и функцию в этом коде.
 
 interface ITask {
-  // Типизируйте объект Task
+  id: number;
+  title: string;
+  completed: boolean;
+  priority?: 'high' | 'medium' | 'low';
 }
 
 // tasks - должен быть типизирован как массив объектов Task
-const tasks = [
+const tasks : ITask[] = [
   { id: 1, title: 'Купить продукты', completed: true, priority: 'high' },
   { id: 2, title: 'Сделать домашнее задание', completed: false },
   { id: 3, title: 'Сходить в спортзал', completed: false },
@@ -17,11 +20,11 @@ const tasks = [
   { id: 8, title: 'Позвонить маме', completed: true, priority: 'high' },
   { id: 9, title: 'Погулять в парке', completed: false },
   { id: 10, title: 'Сходить на выставку', completed: true },
-];
+] ;
 
 // Напишите функцию, которая принимает массив задач и возвращает только выполненные задачи.
-function filterCompletedTasks() {
-  // Ваш код
+function filterCompletedTasks(tasks: ITask[]): ITask[] {
+  return tasks.filter((task) => task.completed);
 }
 
 const completedTasks = filterCompletedTasks(tasks);
