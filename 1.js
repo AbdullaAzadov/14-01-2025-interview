c8923ba3058d5ccffa7f337d99a13d1793acb05a
Arsen <arsenbarseghyan138@gmail.com>
2025-01-14 10:37:24 +0400

    1.js

--- 1.js
+++ 1.js
@@ -2,27 +2,42 @@
 //  Преобразуйте Pow в чистую функцию.
 //  Напишите что такое чистая функция и его определения.
 
-// Чистая функция - это ...
+// Чистая функция - это ...  возвращает один и тот же результат при одинаковых входных данных.
 
-let globalCounter = 0;
+// let globalCounter = 0;
+
+// function Pow(number, power) {
+//   globalCounter++;
+//   const MathLibraryResult = Math.pow(number, power);
+
+//   fetch('https://jsonplaceholder.typicode.com/posts')
+//     .then((response) => response.json())
+//     .then((data) => {
+//       console.log(data);
+//     });
+
+//   let res = 1;
+//   for (let i = 0; i < power; i++) {
+//     res *= number;
+//   }
+
+//   return 42;
+// }
+
+// Pow(2, 3); // должно вернуть 8 dsafdsafsadfasdfaas
+// Pow(3, 2); // должно вернуть 9
+
 
 function Pow(number, power) {
-  globalCounter++;
-  const MathLibraryResult = Math.pow(number, power);
+  if (power === 0) return 1; 
+  if (power < 0) return 1 / Pow(number, -power); 
 
-  fetch('https://jsonplaceholder.typicode.com/posts')
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-    });
-
-  let res = 1;
+  let result = 1;
   for (let i = 0; i < power; i++) {
-    res *= number;
+    result *= number;
   }
-
-  return 42;
+  return result;
 }
 
-Pow(2, 3); // должно вернуть 8 dsafdsafsadfasdfaas
-Pow(3, 2); // должно вернуть 9
+console.log(Pow(2, 3)); 
+console.log(Pow(3, 2));
