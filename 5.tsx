99881f1d1fe383faf73b4087ff385c2b876111e4
ElamanBaiguanishev <elaman1007@mail.ru>
2025-01-14 12:47:41 +0600

    5 task complete

--- 5.tsx
+++ 5.tsx
@@ -14,11 +14,48 @@ desktop - если ширина экрана больше 1024px
 */
 
 // TScreenType
+type TScreenType = 'mobile' | 'tablet' | 'desktop';
 
 // getCurrentScreen - функция, которая возвращает строку с типом экрана
+const getCurrentScreen = (width: number): TScreenType => {
+    if (width < 768) {
+        return 'mobile';
+    } else if (width < 1024) {
+        return 'tablet';
+    } else {
+        return 'desktop';
+    }
+};
 
 // ScreenContext
+const ScreenContext = createContext<TScreenType | undefined>(undefined);
 
 // ScreenContextProvider
+const ScreenContextProvider: React.FC = ({ children }) => {
+    const [screenType, setScreenType] = useState<TScreenType>('mobile');
+
+    useEffect(() => {
+        setScreenType(getCurrentScreen(window.innerWidth)); // Устанавливаем тип экрана при инициализации
+    }, []);
+
+    return (
+        <ScreenContext.Provider value={screenType}>
+            {children}
+        </ScreenContext.Provider>
+    );
+};
+
 
 // useScreenType
+const useScreenType = (): TScreenType => {
+    const screenType = useContext(ScreenContext);
+
+    if (screenType === undefined) {
+        throw new Error('useScreen must be used within a ScreenProvider');
+    }
+
+    return screenType;
+};
+
+
+
