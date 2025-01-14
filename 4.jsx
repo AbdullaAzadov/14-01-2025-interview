d6f33b917cb8c1ab9bea6915350e85cf1f36eaf6
Abzalbek Ibrokhimov <ibrohimov04@bk.ru>
2025-01-14 11:45:57 +0500

    4-task

--- 4.jsx
+++ 4.jsx
@@ -12,31 +12,26 @@ import { useState, useEffect } from 'react';
 
 const Component = () => {
   const [count, setCount] = useState(0);
-  const [isGreaterThan5, setIsGreaterThan5] = useState(false);
 
-  useEffect(() => {
-    if (count > 5) {
-      setIsGreaterThan5(true);
-    } else {
-      setIsGreaterThan5(false);
-    }
-  }, [count]);
-
-  if (count > 5) {
     useEffect(() => {
       const handleResize = () => {
+        if (count > 5)
         console.log('Resize event detected');
       };
       window.addEventListener('resize', handleResize);
-    }, []);
-  }
+      return () => {
+        window.removeEventListener('resize', handleResize);
+      };
+    }, [count]); 
+
+
 
   return (
-    <div>
-      <p>Счетчик: {count}</p>
-      <button onClick={() => setCount(count + 1)}>Увеличить</button>
-      <button onClick={() => setCount(count - 1)}>Уменьшить</button>
-      {isGreaterThan5 && <p>Счетчик больше 5</p>}
-    </div>
+      <div>
+        <p>Счетчик: {count}</p>
+        <button onClick={() => setCount((prev) => prev + 1)}>Увеличить</button>
+        <button onClick={() => setCount((prev) => prev - 1)}>Уменьшить</button>
+        {count > 5 && <p>Счетчик больше 5</p>}
+      </div>
   );
 };
