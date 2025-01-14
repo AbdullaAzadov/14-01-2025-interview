ba1b7fca159aec36af7e0851bbc43ea0ce9f919c
teriyaki0 <ttemyrlan5@gmail.com>
2025-01-14 11:40:32 +0500

    Решена задачи 3

--- 3.tsx
+++ 3.tsx
@@ -2,30 +2,51 @@
 Реализуйте компонент UserSearch, который позволяет искать пользователей по имени.
 Также необходимо типизировать объект User и компонент UserSearch.
 */
+import React, { useState } from "react"; // импортируем все что нужно нам из библиотеки реакт
 
 interface IUser {
-  // Типизируйте объект User
+  id: number;
+  name: string;
+  age: number;
 }
 
-// Типизируйте компонент UserSearch
-const UserSearch = () => {
-  const users = [
-    { id: 1, name: 'Иван', age: 25 },
-    { id: 2, name: 'Мария', age: 30 },
-    { id: 3, name: 'Петр', age: 28 },
-    { id: 4, name: 'Алексей', age: 35 },
-    { id: 5, name: 'Анна', age: 22 },
+const UserSearch: React.FC = () => {
+  // Массив с типизацией IUser[]
+  const users: IUser[] = [
+    { id: 1, name: "Иван", age: 25 },
+    { id: 2, name: "Мария", age: 30 },
+    { id: 3, name: "Петр", age: 28 },
+    { id: 4, name: "Алексей", age: 35 },
+    { id: 5, name: "Анна", age: 22 },
   ];
 
-  const [searchTerm, setSearchTerm] = useState</*типизируйте useState*/>('');
-  const filteredUsers = []; // Фильтруйте пользователей по имени
+  // Типизация useState где мы используем generic
+  const [searchTerm, setSearchTerm] = useState<string>("");
+
+  const filteredUsers = users.filter((user) =>
+    // мы специально делаем входные данные в нижнем регистре и проверяем так же в нем ведь это улучшает подьзовательский опыт
+    user.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
   return (
     <div>
       <h1>Поиск пользователей</h1>
-      {/* Здесь напишите input для посика */}
+      {/* Input для ввода поискового запроса */}
+      <input
+        type="text"
+        placeholder="Введите имя"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
 
-      <ul>{/* Выводите список отфильтрованных пользователей здесь */}</ul>
+      <ul>
+        {/* Выводим список отфильтрованных пользователей */}
+        {filteredUsers.map((user) => (
+          <li key={user.id}>
+            {user.name}, {user.age} лет
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
