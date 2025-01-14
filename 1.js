9c83756b1b4edda3831d58a04950c3c050451481
Vova <tarabanvv@mail.ru>
2025-01-14 14:26:09 +0800

    начал

--- 1.js
+++ 1.js
@@ -2,27 +2,15 @@
 //  Преобразуйте Pow в чистую функцию.
 //  Напишите что такое чистая функция и его определения.
 
-// Чистая функция - это ...
-
-let globalCounter = 0;
+// Чистая функция - это функция которая возвращает одинаковый результат при одних и тех же входных данных + в ней нет побочнх эффектов
 
 function Pow(number, power) {
-  globalCounter++;
-  const MathLibraryResult = Math.pow(number, power);
-
-  fetch('https://jsonplaceholder.typicode.com/posts')
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-    });
-
-  let res = 1;
-  for (let i = 0; i < power; i++) {
-    res *= number;
-  }
-
-  return 42;
+	let res = 1;
+	for (let i = 0; i < power; i++) {
+		res *= number;
+	}
+	return res;
 }
 
-Pow(2, 3); // должно вернуть 8 dsafdsafsadfasdfaas
+Pow(2, 3); // должно вернуть 8
 Pow(3, 2); // должно вернуть 9
--- 2.ts
+++ 2.ts
@@ -2,26 +2,39 @@
 // Типизируйте данные и функцию в этом коде.
 
 interface ITask {
-  // Типизируйте объект Task
+	id: number;
+	title: string;
+	completed: boolean;
+	priority?: Priority;
+}
+
+enum Priority {
+	Low = 'low',
+	Medium = 'medium',
+	High = 'high'
 }
 
 // tasks - должен быть типизирован как массив объектов Task
-const tasks = [
-  { id: 1, title: 'Купить продукты', completed: true, priority: 'high' },
-  { id: 2, title: 'Сделать домашнее задание', completed: false },
-  { id: 3, title: 'Сходить в спортзал', completed: false },
-  { id: 4, title: 'Прочитать книгу', completed: true, priority: 'low' },
-  { id: 5, title: 'Помыть машину', completed: false },
-  { id: 6, title: 'Погулять с собакой', completed: true, priority: 'medium' },
-  { id: 7, title: 'Сделать уборку', completed: false },
-  { id: 8, title: 'Позвонить маме', completed: true, priority: 'high' },
-  { id: 9, title: 'Погулять в парке', completed: false },
-  { id: 10, title: 'Сходить на выставку', completed: true },
+const tasks: ITask[] = [
+	{ id: 1, title: 'Купить продукты', completed: true, priority: Priority.High },
+	{ id: 2, title: 'Сделать домашнее задание', completed: false },
+	{ id: 3, title: 'Сходить в спортзал', completed: false },
+	{ id: 4, title: 'Прочитать книгу', completed: true, priority: Priority.Low },
+	{ id: 5, title: 'Помыть машину', completed: false },
+	{
+		id: 6,
+		title: 'Погулять с собакой',
+		completed: true,
+		priority: Priority.Medium
+	},
+	{ id: 7, title: 'Сделать уборку', completed: false },
+	{ id: 8, title: 'Позвонить маме', completed: true, priority: Priority.High },
+	{ id: 9, title: 'Погулять в парке', completed: false },
+	{ id: 10, title: 'Сходить на выставку', completed: true }
 ];
-
 // Напишите функцию, которая принимает массив задач и возвращает только выполненные задачи.
 function filterCompletedTasks() {
-  // Ваш код
+	// Ваш код
 }
 
 const completedTasks = filterCompletedTasks(tasks);
