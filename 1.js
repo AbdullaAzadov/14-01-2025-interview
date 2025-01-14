30fee08e6b1f2c81b1a5af75843218c2ed1790dc
Inga <ingatkachuk8@gmail.com>
2025-01-14 11:27:20 +0500

    First task

--- 1.js
+++ 1.js
@@ -2,26 +2,15 @@
 //  Преобразуйте Pow в чистую функцию.
 //  Напишите что такое чистая функция и его определения.
 
-// Чистая функция - это ...
+// Чистая функция - это фунцкия
 
-let globalCounter = 0;
 
 function Pow(number, power) {
-  globalCounter++;
-  const MathLibraryResult = Math.pow(number, power);
-
-  fetch('https://jsonplaceholder.typicode.com/posts')
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-    });
-
   let res = 1;
   for (let i = 0; i < power; i++) {
     res *= number;
   }
-
-  return 42;
+  return res;
 }
 
 Pow(2, 3); // должно вернуть 8 dsafdsafsadfasdfaas
