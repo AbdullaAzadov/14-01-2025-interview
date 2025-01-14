a0489db65a6bf145b6a445169f5bd99b797afb00
NikitaSimakov <mishaaniskin@gmail.com>
2025-01-14 09:23:52 +0300

    init

--- 2.ts
+++ 2.ts
@@ -7,16 +7,16 @@ interface ITask {
 
 // tasks - должен быть типизирован как массив объектов Task
 const tasks = [
-  { id: 1, title: 'Купить продукты', completed: true, priority: 'high' },
-  { id: 2, title: 'Сделать домашнее задание', completed: false },
-  { id: 3, title: 'Сходить в спортзал', completed: false },
-  { id: 4, title: 'Прочитать книгу', completed: true, priority: 'low' },
-  { id: 5, title: 'Помыть машину', completed: false },
-  { id: 6, title: 'Погулять с собакой', completed: true, priority: 'medium' },
-  { id: 7, title: 'Сделать уборку', completed: false },
-  { id: 8, title: 'Позвонить маме', completed: true, priority: 'high' },
-  { id: 9, title: 'Погулять в парке', completed: false },
-  { id: 10, title: 'Сходить на выставку', completed: true },
+  { id: 1, title: "Купить продукты", completed: true, priority: "high" },
+  { id: 2, title: "Сделать домашнее задание", completed: false },
+  { id: 3, title: "Сходить в спортзал", completed: false },
+  { id: 4, title: "Прочитать книгу", completed: true, priority: "low" },
+  { id: 5, title: "Помыть машину", completed: false },
+  { id: 6, title: "Погулять с собакой", completed: true, priority: "medium" },
+  { id: 7, title: "Сделать уборку", completed: false },
+  { id: 8, title: "Позвонить маме", completed: true, priority: "high" },
+  { id: 9, title: "Погулять в парке", completed: false },
+  { id: 10, title: "Сходить на выставку", completed: true },
 ];
 
 // Напишите функцию, которая принимает массив задач и возвращает только выполненные задачи.
