54be1efb84e88b5d0e4534f06ed6c93d3b9cabee
faxriddinovich <eliboyevasleshka@gmail.com>
2025-01-14 11:47:58 +0500

    Решена задачи 5 и Readme.md

--- 5.tsx
+++ 5.tsx
@@ -13,12 +13,53 @@ desktop - если ширина экрана больше 1024px
 Напишите объяснение к написанному коду.
 */
 
+import React, { createContext, useContext, useState, useEffect, ReactNode } from 'react';
+
 // TScreenType
+type TScreenType = 'mobile' | 'tablet' | 'desktop'; //Тип экрана
 
 // getCurrentScreen - функция, которая возвращает строку с типом экрана
 
+const getCurrentScreen = (): TScreenType => {
+    const width = window.innerWidth;
+    if (width < 768) return 'mobile';
+    if (width < 1024) return 'tablet';
+    return 'desktop';
+};
+
 // ScreenContext
+
+const ScreenContext = createContext<TScreenType | undefined>(undefined); //контекст экрана
 
 // ScreenContextProvider
 
+export const ScreenContextProvider: React.FC<ScreenContextProviderProps> = ({ children }) => {
+    const [screenType, setScreenType] = useState<TScreenType>(getCurrentScreen);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setScreenType(getCurrentScreen());
+        };
+
+
+        window.addEventListener('resize', handleResize);// добавление слушателя изменения размера окна
+
+        return () => {
+            window.removeEventListener('resize', handleResize);// удаление слушателя при размонтировании
+        };
+    }, []);
+
+    return <ScreenContext.Provider value={screenType}>{children}</ScreenContext.Provider>;
+};
+
 // useScreenType
+
+export const useScreenType = (): TScreenType => {
+    const context = useContext(ScreenContext);
+
+    if (context === undefined) {
+        throw new Error('useScreenType must be used within a ScreenContextProvider');// если контекст не найден, выбрасывается ошибка
+    }
+
+    return context;
+}
